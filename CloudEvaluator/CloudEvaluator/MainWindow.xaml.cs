aa21ba12148272e8db1a2e02b69e81287e0e94c1
lokt02 <lokt02@mail.ru>
2020-09-23 20:39:18 +0300

    Client-server

---
+++ CloudEvaluator/CloudEvaluator/MainWindow.xaml.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Research.SEAL;
+using System.Net.Sockets;
+using System.Net;
+using Mathos.Parser;
+
+namespace CloudEvaluator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Button1_click(object sender, RoutedEventArgs e)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                s.Connect(ServerIP.Text, Convert.ToInt32(ServerPort.Text));
+
+
+                string m = TextBox1.Text;
+                Graf_Building(0.1, 20, m, s);
+                MessageBox.Show("clicked");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //try
+            //{
+
+            //    byte[] res_buff = new byte[1024];
+            //    s.Receive(res_buff);
+
+            //    MessageBox.Show(Encoding.ASCII.GetString(res_buff));
+            //}
+            //catch(Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+
+        }
+
+        public void OnCanvasClick(object sender, RoutedEventArgs e)
+        {
+            //string m = TextBox1.Text;
+            //Graf_Building(0.01, 20, m);
+        }
+
+        public void Graf_Building(double step, int m, string func, Socket s)
+        {
+            Canvas.Children.Clear();
+            AxisInit(m);
+
+            Calculation(step, m, func, s);
+        }
+
+        public void Calculation(double step, int m, string func, Socket s)
+        {
+            string val = func + " ";
+            for (double i = -Canvas.ActualWidth / 2; i <= Canvas.ActualWidth / 2; i += step)
+            {
+                val += Math.Round(i, 6).ToString() + " ";
+            }
+            byte[] buffer = Encoding.ASCII.GetBytes(val);
+            s.Send(buffer);
+
+            byte[] buffer1 = new byte[8388608 * 2];
+            s.Receive(buffer1);
+
+            string[] str = Encoding.ASCII.GetString(buffer1).Split(' ');
+            int j = 1;
+            for (double i = -Canvas.ActualWidth / 2; i <= Canvas.ActualWidth / 2; i += step)
+            {
+                double f = Convert.ToDouble(str[j]);
+                j++;
+
+                if (i * m <= Canvas.ActualWidth / 2 && i * m > -Canvas.ActualWidth / 2 && f * m <= Canvas.ActualHeight / 2 && f * m > -Canvas.ActualHeight / 2)
+                {
+                    Point p1 = new Point(i * m + Canvas.ActualWidth / 2, f * m + Canvas.ActualHeight / 2);
+                    Point p2 = new Point((i - step) * m + Canvas.ActualWidth / 2, f * m + Canvas.ActualHeight / 2);
+
+                    Lines(p2, p1);
+
+                }
+            }
+        }
+
+        public void AxisInit(int m)
+        {
+            Line YAxis = new Line
+            {
+                Stroke = Brushes.Red,
+                X1 = Canvas.ActualWidth / 2,
+                X2 = Canvas.ActualWidth / 2,
+                Y1 = 0,
+                Y2 = Canvas.ActualHeight
+            };
+            Canvas.Children.Add(YAxis);
+
+            Point zPoint = new Point(Canvas.ActualWidth / 2, Canvas.ActualHeight / 2);
+
+            for (int i = (int)(Canvas.ActualHeight / 2 + m); i < Canvas.ActualHeight; i+=m)
+            {
+                Line y = new Line
+                {
+                    Stroke = Brushes.Red,
+                    X1 = Canvas.ActualWidth / 2 - m/5,
+                    X2 = Canvas.ActualWidth / 2 + m/5,
+                    Y1 = i,
+                    Y2 = i
+                };
+                Canvas.Children.Add(y);
+            }
+
+            for (int i = (int)(Canvas.ActualHeight / 2 - m); i > 0; i -= m)
+            {
+                Line y = new Line
+                {
+                    Stroke = Brushes.Red,
+                    X1 = Canvas.ActualWidth / 2 - m / 5,
+                    X2 = Canvas.ActualWidth / 2 + m / 5,
+                    Y1 = i,
+                    Y2 = i
+                };
+                Canvas.Children.Add(y);
+            }
+
+            Line XAxis = new Line
+            {
+                Stroke = Brushes.Green,
+                X1 = 0,
+                X2 = Canvas.ActualWidth,
+                Y1 = Canvas.ActualHeight / 2,
+                Y2 = Canvas.ActualHeight / 2
+            };
+            Canvas.Children.Add(XAxis);
+
+            for (int i = (int)(Canvas.ActualWidth / 2 + m); i < Canvas.ActualWidth; i += m)
+            {
+                Line x = new Line
+                {
+                    Stroke = Brushes.Green,
+                    X1 = i,
+                    X2 = i,
+                    Y1 = Canvas.ActualHeight / 2 - m / 5,
+                    Y2 = Canvas.ActualHeight / 2 + m / 5
+                };
+                Canvas.Children.Add(x);
+            }
+
+            for (int i = (int)(Canvas.ActualWidth / 2 - m); i > 0; i -= m)
+            {
+                Line x = new Line
+                {
+                    Stroke = Brushes.Green,
+                    X1 = i,
+                    X2 = i,
+                    Y1 = Canvas.ActualHeight / 2 - m / 5,
+                    Y2 = Canvas.ActualHeight / 2 + m / 5
+                };
+                Canvas.Children.Add(x);
+            }
+        }
+
+        //public double Func(double x, string func)
+        //{
+        //    var res = func.Replace("x", "(" + (Math.Round(x, 4)).ToString().Replace(',','.') + ")");
+        //    return new MathParser().Parse(res);
+        //}
+
+        private void Lines(Point p1, Point p2)
+        {
+            try
+            {
+                Line line = new Line
+                {
+                    X1 = p1.X,
+                    Y1 = p1.Y,
+                    X2 = p2.X,
+                    Y2 = p2.Y,
+                    Stroke = Brushes.Black
+                };
+                Canvas.Children.Add(line);
+            }
+            catch (Exception e)
+            {
+                //Line line = new Line
+                //{
+                //    X1 = p1.X,
+                //    Y1 = -Canvas.ActualHeight,
+                //    X2 = p2.X,
+                //    Y2 = p2.Y,
+                //    Stroke = Brushes.Black
+                //};
+                //Canvas.Children.Add(line);
+            }
+        }
+    }
+}
